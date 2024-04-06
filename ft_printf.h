98207ed256dc8b28ed14c0555920520fbee70fbf
iamdudumon <kksy4511@naver.com>
2024-04-06 19:10:53 +0900

    feat: ft_printh header file c추가

---
+++ ft_printf.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dukim <dukim@student.42gyeonsan.kr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/04 16:29:50 by dukim             #+#    #+#             */
+/*   Updated: 2024/04/04 16:40:47 by dukim            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# define BUFFER_SIZE 42
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdarg.h>
+
+void    ft_putchar(char ch);
+void    ft_putstr(const char *str);
+void	ft_putnbr(int n);
+
+#endif
