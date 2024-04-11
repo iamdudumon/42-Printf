734d6926776439f756984ce2dfe65268cd8b53e1
iamdudumon <kksy4511@naver.com>
2024-04-11 19:07:19 +0900

    fix: "" 빈 문자열 출력시 -1 반환 에러 수정 + helper 함수에 static 사용 + norm 수정을 위해 init_val 메소드 추가

--- ft_printf.c
+++ ft_printf.c
@@ -12,7 +12,7 @@
 
 #include "ft_printf.h"
 
-void	clean_buffer(t_buffer *buf, int *res)
+static void	clean_buffer(t_buffer *buf, int *res)
 {
 	if (buf->len > 0)
 	{
@@ -22,7 +22,7 @@ void	clean_buffer(t_buffer *buf, int *res)
 	}
 }
 
-void	store_buffer(t_buffer *buf, int *res, char ch)
+static void	store_buffer(t_buffer *buf, int *res, char ch)
 {
 	if (buf->len == BUFFER_SIZE)
 		clean_buffer(buf, res);
@@ -31,7 +31,7 @@ void	store_buffer(t_buffer *buf, int *res, char ch)
 	(buf->len)++;
 }
 
-int	print_format(char **str, va_list args)
+static int	print_format(char **str, va_list args)
 {
 	char	*format;
 
@@ -54,7 +54,14 @@ int	print_format(char **str, va_list args)
 		return (ft_putchar('%'));
 	if (*(format + 1) != '\0')
 		return (ft_putchar('%'));
-	return (0);
+	return (-1);
+}
+
+void	init_val(t_buffer *buf, int *res, int *error_flag)
+{
+	buf->len = 0;
+	*res = 0;
+	*error_flag = 1;
 }
 
 int	ft_printf(const char *str, ...)
@@ -65,64 +72,65 @@ int	ft_printf(const char *str, ...)
 	int			error_flag;
 
 	va_start(args, str);
-	buf.len = 0;
-	res = 0;
-	error_flag = 1;
+	init_val(&buf, &res, &error_flag);
 	while (*str != '\0')
 	{
 		if (*str == '%')
 		{
 			clean_buffer(&buf, &res);
 			error_flag = print_format((char **)&str, args);
-			res += error_flag;
+			if (error_flag != -1)
+				res += error_flag;
 			continue ;
 		}
 		store_buffer(&buf, &res, *str);
 		str++;
 	}
 	clean_buffer(&buf, &res);
-	if (error_flag == 0)
+	if (error_flag == -1)
 		return (-1);
 	return (res);
 }
 
 // #include <stdio.h>
 // int main(){
-//     int n1, n2;
+//     // int n1, n2;
 
-//     n1= ft_printf("%d\n", 0);
-//     n2 = printf("%d\n", 0);
-//     printf("%d %d\n", n1, n2);
+//     // n1= ft_printf("%d\n", 0);
+//     // n2 = printf("%d\n", 0);
+//     // printf("%d %d\n", n1, n2);
 
-//     n1= ft_printf("%u\n", 2147483648 + 2147483648 - 1);
-//     n2 = printf("%u\n", 2147483648 + 2147483648 - 1);
-//     printf("%d %d\n", n1, n2);
+//     // n1= ft_printf("%u\n", 2147483648 + 2147483648 - 1);
+//     // n2 = printf("%u\n", 2147483648 + 2147483648 - 1);
+//     // printf("%d %d\n", n1, n2);
 
-//     ft_printf("%%\n", -2);
-//     printf("%%\n", -2);
+//     // ft_printf("%%\n", -2);
+//     // printf("%%\n", -2);
 
-//     ft_printf("%p\n", -1);
-//     printf("%p\n", -1);
+//     // ft_printf("%p\n", -1);
+//     // printf("%p\n", -1);
 
-//     ft_printf("%s\n", 0);
-//     printf("%s\n", 0);
+//     // ft_printf("%s\n", 0);
+//     // printf("%s\n", 0);
 
-//     n1 = printf("123456789012345678901234567890123456789012345678901234567980\n");
-//     n2 = ft_printf("123456789012345678901234567890123456789012345678901234567980\n");
-//     printf("%d %d\n", n1, n2);
+//     // n1 = printf("123456789012345678901234567890123456789012345678901234567980\n");
+//     // n2 = ft_printf("123456789012345678901234567890123456789012345678901234567980\n");
+//     // printf("%d %d\n", n1, n2);
 
-//     n1 = ft_printf("myname is %s\nmy age: %d, my sex: %c\n", "dukim", 25, 'm');
-//     n2 = printf("myname is %s\nmy age: %d, my sex: %c\n", "dukim", 25, 'm');
-//     printf("%d %d\n", n1, n2);
+//     // n1 = ft_printf("myname is %s\nmy age: %d, my sex: %c\n", "dukim", 25, 'm');
+//     // n2 = printf("myname is %s\nmy age: %d, my sex: %c\n", "dukim", 25, 'm');
+//     // printf("%d %d\n", n1, n2);
 
-//     ft_printf("%s\t\t---%p\n", 0, 0);
-//     printf("%s\t\t---%p\n", 0, 0);
+//     // ft_printf("%s\t\t---%p\n", 0, 0);
+//     // printf("%s\t\t---%p\n", 0, 0);
 
-//     ft_printf("%x %X\n", 23408230948230, -111);
-//     printf("%x %X\n", 23408230948230, -111);
+//     // ft_printf("%x %X\n", 23408230948230, -111);
+//     // printf("%x %X\n", 23408230948230, -111);
 
-//     n1 = ft_printf("%d %z", 5, 5);
-//     printf("%d\n", n1);
-//     n2 = printf("%d %z", 5, 5);
-//     printf("%d\n", n2);
+//     // n1 = ft_printf("%d %z", 5, 5);
+//     // printf("%d\n", n1);
+//     // n2 = printf("%d %z", 5, 5);
+//     // printf("%d\n", n2);
+// 	ft_printf("%z \n");
+// 	printf("%z \n");
 // }
