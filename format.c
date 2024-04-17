5fdd1e0b2a675a0ba33df07f4eb6836c7a71d984
iamdudumon <kksy4511@naver.com>
2024-04-17 21:05:44 +0900

    이것저것 수정...

--- format.c
+++ format.c
@@ -12,44 +12,21 @@
 
 #include "ft_printf.h"
 
-t_format    make_format(const char *str)
+t_specifier *set_specifier(char type, va_list args)
 {
-    t_format    format;
-    int         width_len;
-    
-    format.flag_cnt = 0;
-    while (*str == "-" || *str == '+' || *str == '0')
-    {
-        format.flag[format.flag_cnt++] = *str;
-        *str++;
-    }
-    width_len = 0;
-    while ('0' <= *str && *str <= '0')
-        width_len++;
-    format.width = ft_atoi(*str, width_len);
-    str += width_len;
-    set_specifier(str, &(format.spec), *str);
-    return (format);
-}
-
-void	set_specifier(const char *str, t_type *spec, char type)
-{
-	if (type == '\0')
-        spec->len = 0;
     if (type == 'c')
-		spec = ft_putchar(va_arg(args, int));
+		return ft_putchar(va_arg(args, int));
 	if (type == 's')
-		spec = ft_putstr(va_arg(args, char *));
+		return ft_putstr(va_arg(args, char *));
 	if (type == 'd' || type == 'i' || type == 'u')
-		spec = ft_putnbr(va_arg(args, int), type != 'u');
+		return ft_putnbr(va_arg(args, int), type != 'u');
 	if (type == 'p')
-		spec = ft_putaddr(va_arg(args, void *));
+		return ft_putaddr(va_arg(args, void *));
 	if (type == 'X' || type == 'x')
-		spec = ft_puthex(va_arg(args, unsigned int), type == 'x', 0);
+		return ft_puthex(va_arg(args, unsigned int), type == 'x', 0);
 	if (type == '%')
-		spec = ft_putchar('%');
-	if (*(str + 1) != '\0')
-		spec = ft_putchar('%');
+		return ft_putchar('%');
+    return (0);
 }
 
 void    print_width(int width, int zero_flag)
@@ -64,4 +41,28 @@ void    print_width(int width, int zero_flag)
         write(1, &ch, 1);
         width--;
     }
+}
+
+t_format    make_format(const char *str, va_list args)
+{
+    t_format    format;
+
+    format.flag_cnt = 0;
+    format.width_len = 0;
+    format.error_flag = 0;
+    while (*str == '-' || *str == '+' || *str == '0')
+    {
+        format.flag[format.flag_cnt++] = *str;
+        str++;
+    }
+    while ('0' <= *str && *str <= '0')
+    {
+        format.width_len++;
+        str++;
+    }
+    format.width = ft_atoi(str - format.width_len, format.width_len);
+    format.spec = set_specifier(*str, args);
+    if (!format.spec)
+        format.error_flag = 1;
+    return (format);
 }
--- ft_printf.h
+++ ft_printf.h
@@ -27,9 +27,8 @@ typedef struct s_buffer
 
 typedef struct s_specifier
 {
-	// char	specifier;
 	char	*str;
-	size_t	len;
+	int		len;
 }			t_specifier;
 
 typedef struct s_format
@@ -37,23 +36,26 @@ typedef struct s_format
 	char		flag[3];
 	int			flag_cnt;
 	int			width;
-	t_specifier	spec;
+	int			width_len;
+	char		error_flag;
+	t_specifier	*spec;
 }			t_format;
 
-int		ft_printf(const char *str, ...);
+int			ft_printf(const char *str, ...);
 
-int		ft_putchar(const char ch);
-int		ft_putstr(const char *str);
-int		ft_putnbr(int n, int sign_flag);
-int		ft_putaddr(const void *addr);
-int		ft_puthex(unsigned long n, int case_flag, int addr_flag);
+t_specifier	*ft_putchar(const char ch);
+t_specifier	*ft_putstr(const char *str);
+t_specifier	*ft_putnbr(int n, int sign_flag);
+t_specifier	*ft_putaddr(const void *addr);
+t_specifier	*ft_puthex(unsigned long n, int case_flag, int addr_flag);
 
-int		pow_ten(unsigned int un);
-size_t	ft_strlen(const char *str);
-int		ft_atoi(const char *nptr);
-char	*ft_strdup(const char *s);
+int			pow_ten(unsigned int un);
+int		ft_strlen(const char *str);
+int			ft_atoi(const char *nptr, int len);
+char		*ft_strdup(const char *s);
+char		*ft_strjoin(const char *s1, const char *s2);
 
-t_format    get_format(char *str);
-void    print_width(int width, int zero_flag);
+t_format    make_format(const char *str, va_list args);
+void    	print_width(int width, int zero_flag);
 
 #endif
