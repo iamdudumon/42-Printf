6821b3e005dd55694a4a73013084ec5ab1301357
iamdudumon <kksy4511@naver.com>
2024-04-20 20:39:36 +0900

    feat: libft 라이브러리 추가

--- ft_printf.h
+++ ft_printf.h
@@ -13,11 +13,12 @@
 #ifndef FT_PRINTF_H
 #define FT_PRINTF_H
 
-#define BUFFER_SIZE 42
+# define BUFFER_SIZE 42
 
-#include <stdlib.h>
-#include <unistd.h>
-#include <stdarg.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdarg.h>
+# include "libft/libft.h"
 
 typedef struct s_buffer
 {
@@ -38,10 +39,12 @@ typedef struct s_format
 	char		plus_flag;
 	char		minus_flag;
 	char		zero_flag;
+	char		precision_flag;
 	char		error_flag;
 	int			width;
+	int			precision;
 	int			size;
-	t_specifier	*spec;
+	t_specifier	spec;
 }				t_format;
 
 int			ft_printf(const char *str, ...);
@@ -50,19 +53,20 @@ void		clean_buffer(t_buffer *buf, int *res);
 void		store_buffer(t_buffer *buf, int *res, char ch);
 
 t_specifier	*ft_putchar(const char ch);
-t_specifier *ft_putstr(const char *str);
+// t_specifier *ft_putstr(const char *str);
 t_specifier *ft_putnbr(int n, int sign_flag);
 t_specifier *ft_putaddr(const void *addr);
 t_specifier *ft_puthex(unsigned long n, int case_flag, int addr_flag);
 
+t_specifier	ft_putstr(t_format format, const char *str);
+
 int			pow_ten(unsigned int un);
-int			ft_strlen(const char *str);
-int			ft_atoi(const char *nptr, int len);
-char		*ft_strdup(const char *s);
-char		*ft_strjoin(const char *s1, const char *s2);
-void		*ft_memset(void *s, int c, size_t n);
+// int			ft_strlen(const char *str);
+// int			ft_atoi(const char *nptr, int len);
+// char		*ft_strdup(const char *s);
+// char		*ft_strjoin(const char *s1, const char *s2);
+// void		*ft_memset(void *s, int c, size_t n);
 
 t_format	make_format(const char *str, va_list args);
-void		print_width(int width, int zero_flag);
 
 #endif
--- print_utils.c
+++ print_utils.c
@@ -12,15 +12,15 @@
 
 #include "ft_printf.h"
 
-int	ft_strlen(const char *str)
-{
-	const char	*ori;
+// int	ft_strlen(const char *str)
+// {
+// 	const char	*ori;
 
-	ori = str;
-	while (*str != '\0')
-		str++;
-	return (str - ori);
-}
+// 	ori = str;
+// 	while (*str != '\0')
+// 		str++;
+// 	return (str - ori);
+// }
 
 int	pow_ten(unsigned int un)
 {
@@ -34,82 +34,82 @@ int	pow_ten(unsigned int un)
 	return (pow / 10);
 }
 
-int	ft_atoi(const char *nptr, int len)
-{
-	int	sum;
+// int	ft_atoi(const char *nptr, int len)
+// {
+// 	int	sum;
 
-	if (len == 0)
-		return (0);
-	sum = 0;
-	while (len > 0 && ('0' <=*nptr && *nptr <= '9'))
-	{
-		sum = sum * 10 + (*nptr - '0');
-		nptr++;
-		len--;
-	}
-	return (sum);
-}
+// 	if (len == 0)
+// 		return (0);
+// 	sum = 0;
+// 	while (len > 0 && ('0' <=*nptr && *nptr <= '9'))
+// 	{
+// 		sum = sum * 10 + (*nptr - '0');
+// 		nptr++;
+// 		len--;
+// 	}
+// 	return (sum);
+// }
 
-char	*ft_strdup(const char *s)
-{
-	char	*dup;
-	size_t	len;
+// char	*ft_strdup(const char *s)
+// {
+// 	char	*dup;
+// 	size_t	len;
 
-	len = ft_strlen(s);
-	dup = (char *)malloc(sizeof(char) * (len + 1));
-	if (!dup)
-		return (0);
-	while (*s != '\0')
-	{
-		*dup = *s;
-		dup++;
-		s++;
-	}
-	*dup = *s;
-	return (dup - len);
-}
+// 	len = ft_strlen(s);
+// 	dup = (char *)malloc(sizeof(char) * (len + 1));
+// 	if (!dup)
+// 		return (0);
+// 	while (*s != '\0')
+// 	{
+// 		*dup = *s;
+// 		dup++;
+// 		s++;
+// 	}
+// 	*dup = *s;
+// 	return (dup - len);
+// }
 
-char	*ft_strjoin(const char *s1, const char *s2)
-{
-	char	*join_str;
-	int		s1_len;
-	int		s2_len;
+// char	*ft_strjoin(const char *s1, const char *s2)
+// {
+// 	char	*join_str;
+// 	int		s1_len;
+// 	int		s2_len;
 
-	s1_len = ft_strlen(s1);
-	s2_len = ft_strlen(s2);
-	join_str = (char *)malloc(sizeof(char) * (s1_len + s2_len + 1));
-	if (!join_str)
-		return (0);
-	while (*s1 != '\0')
-	{
-		*join_str = *s1;
-		join_str++;
-		s1++;
-	}
-	while (*s2 != '\0')
-	{
-		*join_str = *s2;
-		join_str++;
-		s2++;
-	}
-	*join_str = '\0';
-	return (join_str - (s1_len + s2_len));
-}
+// 	s1_len = ft_strlen(s1);
+// 	s2_len = ft_strlen(s2);
+// 	join_str = (char *)malloc(sizeof(char) * (s1_len + s2_len + 1));
+// 	if (!join_str)
+// 		return (0);
+// 	while (*s1 != '\0')
+// 	{
+// 		*join_str = *s1;
+// 		join_str++;
+// 		s1++;
+// 	}
+// 	while (*s2 != '\0')
+// 	{
+// 		*join_str = *s2;
+// 		join_str++;
+// 		s2++;
+// 	}
+// 	*join_str = '\0';
+// 	return (join_str - (s1_len + s2_len));
+// }
 
 
-void	*ft_memset(void *s, int c, size_t n)
-{
-	unsigned char	*convert_s;
-	unsigned char	convert_c;
-	size_t			size;
+// void	*ft_memset(void *s, int c, size_t n)
+// {
+// 	unsigned char	*convert_s;
+// 	unsigned char	convert_c;
+// 	size_t			size;
 
-	convert_s = (unsigned char *)s;
-	convert_c = (unsigned char)c;
-	size = 0;
-	while (size < n)
-	{
-		convert_s[size] = convert_c;
-		size++;
-	}
-	return ((void *)convert_s);
-}
+// 	convert_s = (unsigned char *)s;
+// 	convert_c = (unsigned char)c;
+// 	size = 0;
+// 	while (size < n)
+// 	{
+// 		convert_s[size] = convert_c;
+// 		size++;
+// 	}
+// 	return ((void *)convert_s);
+// }
